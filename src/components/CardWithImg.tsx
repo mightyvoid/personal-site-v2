68c9d492fcd7582d49817567efa078064417677e
Navin <navin@Navins-MacBook-Air.local>
2024-09-24 15:52:10 +0530

    mobile responsiveness

--- src/components/CardWithImg.tsx
+++ src/components/CardWithImg.tsx
@@ -15,7 +15,7 @@ const Card: React.FC<CardProps> = ({ imgSrc, description, link }) => {
       <img src={imgSrc} alt="Card Image" className="card-image" />
       <div className="card-content">
         <p>{description}</p>
-        <a href={link} target="_blank" rel="noopener noreferrer">
+        <a href={link} target="_blank" rel="noopener noreferrer" id="card-checkout-link">
           Check out
         </a>
       </div>
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
 export const scrollIntoView = (id:string="") => {
     const element = document.getElementById(id);
-    console.log("element",element,id)
     element?.scrollIntoView({
         behavior:'smooth'
     })
